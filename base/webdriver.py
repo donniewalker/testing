3700a28692e9053397a02cf79bfe963453b85cac
donniewalker <donalddeanwalker@gmail.com>
2019-02-13 08:02:14 -0600

    First Commit

---
+++ base/webdriver.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+
+class WebDriverInstance():
+
+    def getWebDriverInstance(self):
+        driver = self.driver
+        baseURL = "https://letskodeit.teachable.com/"
+        # Instantiate webdriver
+        driver = webdriver.Chrome()
+        # Setting Driver Implicit Time out for An Element
+        driver.implicitly_wait(3)
+        # Maximize the window
+        driver.maximize_window()
+        # Loading browser with App URL
+        driver.get(baseURL)
+        return driver
