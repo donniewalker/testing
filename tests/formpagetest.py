3bdd7c9492ad4c8fe99fd858349c1cf8e044051d
donniewalker <donalddeanwalker@gmail.com>
2019-03-14 12:56:19 -0500

    form page/test page created

--- tests/formpagetest.py
+++ tests/formpagetest.py
@@ -6,9 +6,11 @@
 
 import logging
 import unittest
+import time
 
 from base.webdriver import WebDriver
 from pages.loginpage import LoginPage
+from pages.formpage import FormPage
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
                     level=logging.INFO)
@@ -21,23 +23,20 @@ class LoginTest(unittest.TestCase):
         cls.webdriver_instance = WebDriver()
         cls.webdriver_instance.getWebDriverInstance()
         cls.driver = cls.webdriver_instance.driver
+        # log_in = LoginPage(cls.driver)
+        # log_in.login_page("test-ctbawsg8setw@example.com", "y$9lB0|YDD")
 
-    def test_one_login_valid(self):
-        logging.info("##### BEGIN VALID LOGIN TEST #####")
-        log_in = LoginPage(self.driver)
-        log_in.login_page("test-ctbawsg8setw@example.com", "y$9lB0|YDD")
-        result = log_in.is_valid()
-        self.assertTrue(result)
-        print("Valid Login Test Passed")
-
-    def test_two_login_invalid(self):
-        logging.info("##### BEGIN INVALID LOGIN TEST #####")
-        log_in = LoginPage(self.driver)
-        log_in.invalid_login_page("test-invalid@login.com", "login")
-        result = log_in.is_invalid()
-        self.assertEqual(result, "Please check your username and password. "
-                         "If you still can't log in, contact your Salesforce administrator.")
-        print("Invalid Login Test Passed")
+    def test_submit_guestcard(self):
+        logging.info("##### BEGIN SUBMIT FULL FORM #####")
+        self.driver.get("https://sandbox-efficiency-page-8761-1694a6059cf.cs69.force.com/s/"
+                        "guest-card?moc=On_Site&propertyId=a1B2D0000003JtUUAU&pNum=679")
+        time.sleep(3)
+        submit_full_form = FormPage(self.driver)
+        submit_full_form.submit_form_all("Henry", "Hen", "Madison", "1234 Yorktown St", "New Hampshire", "67567",
+                                         "3546789876", "henry@madison.com")
+        # result = log_in.is_valid()
+        # self.assertTrue(result)
+        # print("Valid Login Test Passed")
 
     @classmethod
     def tearDownClass(cls):
